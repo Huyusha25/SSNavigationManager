d90cf637c7c4c3d78032b22a2bf207a1aa509f2b
hys <huyusha@live.com>
2020-04-16 11:44:01 +0800

    4.16

--- SSNavManager/OneViewController.m
+++ SSNavManager/OneViewController.m
@@ -21,14 +21,4 @@
     self.ss_prefersNavigationBarAlpha = 0.2;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
